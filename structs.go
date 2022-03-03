f5d818b653d5490c81c8612b983565cb5eb88ea2
Andrew Batallones <batallonesa@gmail.com>
2022-03-03 13:51:31 -0800

    add methods to stucts

--- structs.go
+++ structs.go
@@ -9,6 +9,20 @@ type person struct {
 	age  int
 }
 
+// You can attach methods to structs
+type rect struct {
+	width, height int
+}
+
+// You can pass a method by value or by reference
+func (r *rect) area() int {
+	return r.width * r.height
+}
+
+func (r rect) perim() int {
+	return r.width + r.height
+}
+
 func newPerson(name string) *person {
 	p := person{name: name}
 	p.age = 42
@@ -21,4 +35,9 @@ func main() {
 	fmt.Println(person{name: "Amanda", age: 19})
 
 	fmt.Println(*newPerson("Testington"))
+	fmt.Println("---")
+
+	r := rect{42, 42}
+	fmt.Println(r.area())
+	fmt.Println(r.perim())
 }
