4571392ca4f68fc7d378f9e89542e6a4ea47e49f
Andrew Batallones <batallonesa@gmail.com>
2022-03-03 07:37:42 -0800

    add pointers

---
+++ pointers.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// Go supports points
+// If you want a good explination, I would suggest this video: https://www.youtube.com/watch?v=DTxHyVn0ODg&t=645s
+// * dereferences the pointer
+// & gets the memory address of the value
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(ptr *int) {
+	*ptr = 0
+}
+
+func main() {
+	i := 1
+	fmt.Println(i)
+
+	zeroval(i)
+	fmt.Println(i)
+
+	zeroptr(&i) // this is a pass by reference, so the i value that was declared changes
+	fmt.Println(i)
+
+	fmt.Println(&i)
+}
