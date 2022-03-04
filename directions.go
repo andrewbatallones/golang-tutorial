2de90941482da29687b82762f60606e366c7f404
Andrew Batallones <batallonesa@gmail.com>
2022-03-04 14:24:10 -0800

    add ch directions

---
+++ directions.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// chan<- incomming channel param
+// <- outgoing channel param
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pings <-chan string, pongs chan<- string) {
+	msg := <-pings // instanciating a new input stream
+	pongs <- msg
+}
+
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+
+	ping(pings, "passed message")
+	pong(pings, pongs)
+
+	fmt.Println(<-pongs)
+}
