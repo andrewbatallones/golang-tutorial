acb36961ff25eb2f329bd5243f572d9314d723e1
Andrew Batallones <batallonesa@gmail.com>
2022-03-04 15:25:45 -0800

    add non-blocking

---
+++ non-blocking.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// The default statement in the select makes it non-blocking
+// Basically it will check if the channel has a value and move on to the next statement
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message recieved")
+	}
+
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	case sig := <-signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
